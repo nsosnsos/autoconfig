ecc1a848c12947abf814215f94f634027c2eae51
nsosnsos <nsosnsos@gmail.com>
2020-02-06 18:35:06 +0800

    FEAT: CREATE DATABASE AND RUN CHROME IN BACKGROUND
    create database for mariadb initialization, run chrome in background,
           and make apt-get installation running automatically.

--- raspbian_init.sh
+++ raspbian_init.sh
@@ -15,15 +15,15 @@ sudo passwd root
 su root
 exit
 
-sudo apt-get install xrdp
-sudo apt-get install ntpdate
+sudo apt-get install -y xrdp
+sudo apt-get install -y ntpdate
 sudo ntpdate -u ntp.ubuntu.com
 
-sudo apt-get install ttf-wqy-zenhei ttf-wqy-microhei xfonts-wqy
-sudo apt-get install fcitx fcitx-googlepinyin fcitx-module-cloudpinyin fcitx-sunpinyin
+sudo apt-get install -y ttf-wqy-zenhei ttf-wqy-microhei xfonts-wqy
+sudo apt-get install -y fcitx fcitx-googlepinyin fcitx-module-cloudpinyin fcitx-sunpinyin
 
-sudo apt-get install vim vim-scripts vim-doc vim-addon-manager
-sudo apt-get install ctags cscope tree
+sudo apt-get install -y vim vim-scripts vim-doc vim-addon-manager
+sudo apt-get install -y ctags cscope tree
 vim-addons install omnicppcomplete
 vim-addons install minibufexplorer
 vim-addons install winmanager
@@ -37,12 +37,12 @@ sudo echo trusted-host = pypi.tuna.tsinghua.edu.cn >> /etc/pip.conf
 sudo echo extra-index-url = https://www.piwheels.org/simple https://pypi.org/simple/ >> /etc/pip.conf
 sudo chmod 644 /etc/pip.conf
 
-sudo apt-get install python3-pip
+sudo apt-get install -y python3-pip
 sudo pip3 install --upgrade pip
 rm -rf /usr/bin/pip3
 ln -s /usr/local/bin/pip3 /usr/bin/pip3
 
-sudo apt-get install python3-dev libxml2-dev libxslt1-dev zlib1g-dev libhdf5-dev libblas-dev liblapack-dev libatlas-base-dev gfortran
+sudo apt-get install -y python3-dev libxml2-dev libxslt1-dev zlib1g-dev libhdf5-dev libblas-dev liblapack-dev libatlas-base-dev gfortran
 sudo pip3 install wrapt --ignore-installed
 sudo pip3 install pandas numpy scipy tensorflow matplotlib
 
@@ -57,20 +57,21 @@ git config --global core.excludesFile ~/.gitignore
 git config --global core.editor vim
 git config --global merge.tool vimdiff
 
-sudo apt-get install mariadb-server
+sudo apt-get install -y mariadb-server
 sudo mysql << EOF
 USE mysql;
 UPDATE user SET password=password('root') WHERE user='root';
 UPDATE user SET plugin='mysql_native_password' WHERE user='root';
+CREATE DATABASE IF NOT EXISTS hallelujah CHARSET utf8 COLLATE utf8_bin;
 FLUSH PRIVILEGES;
 EXIT;
 EOF
 
-sudo apt-get install shadowsocks-qt5
+sudo apt-get install -y shadowsocks-qt5
 sudo cat > ~/proxy.sh <<EOF
 export http_proxy="socks5://127.0.0.1:1080"
 export https_proxy="socks5://127.0.0.1:1080"
 export no_proxy="localhost, 127.0.0.1, 192.168.*"
 EOF
 chmod u+x ~/proxy.sh
-echo "alias chrome='chromium-browser --proxy-server=\"socks5://127.0.0.1:1080\"'" >> ~/.bashrc
+echo "alias chrome='chromium-browser --proxy-server=\"socks5://127.0.0.1:1080\" > /dev/null 2>&1 &'" >> ~/.bashrc
