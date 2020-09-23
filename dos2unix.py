06463b049660ea77b0de200c3f24d79b313b925b
nsos <nsosnsos@gmail.com>
2020-09-23 21:11:17 +0800

    Update dos2unix.py

--- dos2unix.py
+++ dos2unix.py
@@ -6,31 +6,28 @@ import os
 import sys
 
 
-def dos2unix(file_name, ext_list):
-    file_ext = os.path.splitext(os.path.basename(file_name))[1]
-    if file_ext in ext_list:
-        return
-    with open(file_name, "r+", encoding='utf8', errors='ignore', newline='') as f:
-        text = f.read()
+def dos2unix(file_name):
+    with open(file_name, "r+", encoding='utf8', errors='ignore', newline='') as file:
+        text = file.read()
         text = re.sub("\r\n", "\n", text)
-        f.seek(0)
-        f.write(text)
-        f.truncate()
+        file.seek(0)
+        file.write(text)
+        file.truncate()
 
 
 def recursive_process(file_path, ext_list):
     if not os.path.exists(file_path):
         return
     elif not os.path.isdir(file_path):
-        dos2unix(file_path, ext_list)
+        dos2unix(file_path)
         return
-    files = os.listdir(file_path)
-    for f in files:
-        cur_file = os.path.join(file_path, f)
-        if os.path.isdir(cur_file):
-            recursive_process(cur_file, ext_list)
-        else:
-            dos2unix(cur_file, ext_list)
+    for file in os.listdir(file_path):
+        if os.path.splitext(os.path.basename(file))[1] not in ext_list:
+            cur_file = os.path.join(file_path, file)
+            if os.path.isdir(cur_file):
+                recursive_process(cur_file, ext_list)
+            else:
+                dos2unix(cur_file)
 
 
 if __name__ == '__main__':
