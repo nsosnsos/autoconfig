b01a14c1675ceccdedb78233a2cae502a5bab672
nsos <nsosnsos@gmail.com>
2020-09-23 20:26:53 +0800

    Update dos2unix.py

--- dos2unix.py
+++ dos2unix.py
@@ -36,6 +36,7 @@ def recursive_process(file_path, ext_list):
 if __name__ == '__main__':
     if len(sys.argv) <= 1:
         print("Usage: dos2unix file_path")
+        sys.exit(-1)
     exclude_list = []
     if len(sys.argv) > 1:
         exclude_list.extend(sys.argv[2].split(","))
