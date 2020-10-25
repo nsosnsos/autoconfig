e40eb8facbad40425301803419d38ca6a39fe622
shenan <nsosnsos@gmail.com>
2020-10-25 06:19:13 +0000

    FIX: CHECK PARAMS WITH VARIABLES
    check number of parameters, use variables to get script file name.

--- remove_trailing_spaces.py
+++ remove_trailing_spaces.py
@@ -19,10 +19,10 @@ def recursive_process(file_path, ext_list):
     if not os.path.exists(file_path):
         return
     elif not os.path.isdir(file_path):
-        dos2unix(file_path)
+        remove_trailing_spaces(file_path)
         return
     for file in os.listdir(file_path):
-        if os.path.splitext(os.path.basename(file))[1] not in ext_list:
+        if file not in ext_list and os.path.splitext(os.path.basename(file))[1] not in ext_list:
             cur_file = os.path.join(file_path, file)
             if os.path.isdir(cur_file):
                 recursive_process(cur_file, ext_list)
@@ -32,9 +32,9 @@ def recursive_process(file_path, ext_list):
 
 if __name__ == '__main__':
     if len(sys.argv) <= 1:
-        print("Usage: dos2unix file_path [ext_list]")
+        print("Usage: "+os.path.basename(__file__)+"dos2unix file_path [ext_list]")
         sys.exit(-1)
     exclude_list = []
-    if len(sys.argv) > 1:
+    if len(sys.argv) > 2:
         exclude_list.extend(sys.argv[2].split(","))
     recursive_process(sys.argv[1], exclude_list)
