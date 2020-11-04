6eddc70cab17c9d53228cf73b08afa7f0ea58e8d
shenan <nsosnsos@gmail.com>
2020-11-04 15:46:58 +0000

    FIX: REX TEST FOR TRAILING SPACES
    fix bug on testing trailing spaces.

--- remove_trailing_spaces.py
+++ remove_trailing_spaces.py
@@ -9,7 +9,7 @@ import sys
 def remove_trailing_spaces(file_name):
     with open(file_name, "r+", encoding='utf8', errors='ignore', newline='') as file:
         text = file.read()
-        text = re.sub("\s+$", "", text)
+        text = re.sub(r'[ \t]+(\n|\Z)', r'\1', text)
         file.seek(0)
         file.write(text)
         file.truncate()
