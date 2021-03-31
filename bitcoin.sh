5d2086e4f8b803da9349680d633cb824c68e1b71
nsos <nsosnsos@gmail.com>
2021-04-01 01:48:31 +0800

    FEAT: CREATE BITCOIN MINER
    add bitcoin miner service for raspberry pi4.

---
+++ bitcoin.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+sudo apt-get install automake autoconf libcurl4-openssl-dev
+cd ~/Workspace
+git clone https://github.com/pooler/cpuminer.git
+cd cpuminer
+ ./autogen.sh
+./configure CFLAGS="-O3"
+sudo make & sudo make install
+
+./minerd --url=stratum+tcp://stratum.slushpool.com:3333 --userpass=nsos.worker1:password
+
+sudo cat > /etc/systemd/system/miner.service <<EOF
+[Unit]
+Description=BitCoin Mining Service
+After=syslog.target network.target auditd.service
+
+[Service]
+Type=simple
+User=root
+ExecStart=/usr/local/bin/minerd --url=stratum+tcp://stratum.slushpool.com:3333 --userpass=nsos.worker1:password -t 1
+ExecReload=/bin/kill -HUP $MAINPID
+ExecStop=/bin/kill -s QUIT $MAINPID
+PrivateTmp=true
+Restart=always
+RestartSec=120s
+
+[Install]
+WantedBy=multi-user.target
+EOF
