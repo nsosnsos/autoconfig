05a828742f88f408577b1295cf6f3b6bc2aa6d2e
nsosnsos <nsosnsos@gmail.com>
2022-05-22 02:12:24 +0800

    FEAT: ADD NGINX CONFIG
    add nginx configuration for shellinabox.

--- oracle_cloud/cloud_init.sh
+++ oracle_cloud/cloud_init.sh
@@ -4,19 +4,45 @@ set -x
 
 HOME_DIR=$(eval echo ~${SUDO_USER})
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+### Update home config
 cp ${SCRIPT_DIR}/../.gitconfig ${HOME_DIR}/
 cp ${SCRIPT_DIR}/../.gitignore ${HOME_DIR}/
 cp ${SCRIPT_DIR}/../.gitmessage ${HOME_DIR}/
 cp ${SCRIPT_DIR}/../.vimrc ${HOME_DIR}/
 
-sudo apt-get update && sudo apt-get upgrade -y
-sudo apt-get install net-tools ntpdate shellinabox
+### Set bash prompt
+cat >> ${HOME_DIR}/.bashrc <<EOF
 
+# personalized prompt sign
+COLOR_RED='\[\e[1;31m\]'
+COLOR_NULL='\[\e[0m\]'
+PS1="\$COLOR_RED[\u@\h \t] \w\$ \$COLOR_NULL"
+
+EOF
+
+### Update and install software
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install net-tools ntpdate shellinabox nginx -y
+
+### Set timezone and synchronize with ntp server
 sudo ntpdate -u ntp.ubuntu.com
 sudo timedatectl set-timezone "Asia/Hong_Kong"
 
-sudo ufw disable
+### Config ssh
+sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+sudo sed -i 's/#TCPKeepAlive yes/TCPKeepAlive yes/g' /etc/ssh/sshd_config
+sudo service ssh restart
 
+### Stop firewall and disable route rules
+sudo ufw disable
+sudo iptables -P INPUT ACCEPT
+sudo iptables -P FORWARD ACCEPT
+sudo iptables -P OUTPUT ACCEPT
+sudo iptables -F
+sudo apt-get purge netfilter-persistent -y
+
+### Enable tcp bbr
 modprobe tcp_bbr
 echo "tcp_bbr" | sudo tee --append /etc/modules-load.d/modules.conf
 echo "net.core.default_qdisc=fq" | sudo tee --append /etc/sysctl.conf
@@ -27,6 +53,12 @@ sudo sysctl -p
 #sysctl net.ipv4.tcp_congestion_control
 #lsmod | grep bbr
 
+### Install and config v2ray
+cd ${SCRIPT_DIR}/../v2ray
+sudo bash v2ray_config.sh
+cd ${SCRIPT_DIR}
+
+### Config shellinabox
 sudo cat > /etc/default/shellinabox <<EOF
 # Should shellinaboxd start automatically
 SHELLINABOX_DAEMON_START=1
@@ -43,24 +75,8 @@ EOF
 sudo systemctl enable shellinabox
 sudo systemctl start shellinabox
 
-cat >> ${HOME_DIR}/.bashrc <<EOF
+### Config nginx
 
-# personalized prompt sign
-COLOR_RED='\[\e[1;31m\]'
-COLOR_NULL='\[\e[0m\]'
-PS1="\$COLOR_RED[\u@\h \t] \w\$ \$COLOR_NULL"
-
-EOF
-
-sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
-sudo sed -i 's/#TCPKeepAlive yes/TCPKeepAlive yes/g' /etc/ssh/sshd_config
-sudo service ssh restart
-
-sudo iptables -P INPUT ACCEPT
-sudo iptables -P FORWARD ACCEPT
-sudo iptables -P OUTPUT ACCEPT
-sudo iptables -F
-sudo apt-get purge netfilter-persistent -y
 echo "***** CHANGE PASSWORD FOR root & ubuntu *****"
 echo "***** REBOOT PLEASE *****"
 
---
+++ oracle_cloud/nginx_init.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -e
+set -x
+
+HOME_DIR=$(eval echo ~${SUDO_USER})
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+### Check for site certificate
+if [[ ! -f ${SCRIPT_DIR}/site.cert || ! -f ${SCRIPT_DIR}/site.key ]]; then
+    echo "Get site certificate first !!!"
+    exit -1
+fi
+
+### Config nginx
+read -p "Enter SITE_NAME: " SITE_NAME
+if [ -z "${SITE_NAME}" ]; then
+    echo "Error: Empty SITE_NAME !!!"
+    exit -1
+fi
+echo "Configuring nginx with site name: [${SITE_NAME}]"
+NGINX_CONFIG_PATH=/etc/nginx
+HOST_PATH=${HOME_DIR}/${SITE_NAME}
+mkdir -p ${HOST_PATH}/cert
+if [ -f ${NGINX_CONFIG_PATH}/sites-enabled/default ]; then
+    rm ${NGINX_CONFIG_PATH}/sites-enabled/default
+fi
+if [ -f ${NGINX_CONFIG_PATH}/sites-enabled/${SITE_NAME} ]; then
+    rm ${NGINX_CONFIG_PATH}/sites-enabled/${SITE_NAME}
+fi
+sudo cp site.conf ${NGINX_CONFIG_PATH}/sites-available/${SITE_NAME}
+sudo cp site.cert ${HOST_PATH}/cert/${SITE_NAME}.cert
+sudo cp site.key ${HOST_PATH}/cert/${SITE_NAME}.key
+sudo sed -z "s|ssl_prefer_server_ciphers on;\n\n|ssl_prefer_server_ciphers on;\n\tssl_certificate ${HOST_PATH}/cert/${SITE_NAME}.cert;\n\tssl_certificate_key ${HOST_PATH}/cert/${SITE_NAME}.key;\n\n|g" /etc/nginx/nginx.conf | sudo tee /etc/nginx/nginx.conf > /dev/null
+sudo sed -i 's|SITE_NAME|'${SITE_NAME}'|g' ${NGINX_CONFIG_PATH}/sites-available/${SITE_NAME}
+sudo sed -i 's|SITE_DIR|'${HOST_PATH}'|g' ${NGINX_CONFIG_PATH}/sites-available/${SITE_NAME}
+sudo sed -i 's|SITE_CERT|'${HOST_PATH}/cert/${SITE_NAME}.cert'|g' ${NGINX_CONFIG_PATH}/sites-available/${SITE_NAME}
+sudo sed -i 's|SITE_KEY|'${HOST_PATH}/cert/${SITE_NAME}.key'|g' ${NGINX_CONFIG_PATH}/sites-available/${SITE_NAME}
+sudo ln -s ${NGINX_CONFIG_PATH}/sites-available/${SITE_NAME} ${NGINX_CONFIG_PATH}/sites-enabled/${SITE_NAME}
+sudo chown -R www-data:www-data ${HOST_PATH}
+sudo service nginx restart
+
