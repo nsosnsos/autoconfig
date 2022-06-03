c4bcc84dd7b25d7281215fac59f5b4988fb8694f
nsosnsos <nsosnsos@gmail.com>
2022-06-04 00:27:40 +0800

    FEAT: ADD JUPYTER NOTEBOOK
    add jupyter notebook, make cloud initalization script completely.

--- oracle_cloud/cloud_init.sh
+++ oracle_cloud/cloud_init.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
+#set -x
 set -e
-set -x
 
 HOME_PATH=$(eval echo ~${SUDO_USER})
 SCRIPT_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_NAME=$(basename $(readlink -f "${0}"))
+
+CERT_PATH=${HOME_PATH}/cert
+if [ ${#} -gt 1 ]; then
+    echo "Usage: ${SCRIPT_NAME} [DOMAIN_NAME]"
+    echo "       If DOMAIN_NAME is provided, then make sure legtimate certificate(site.cert/site.key) is provided in [${HOME_PATH}/cert/]."
+    echo "       If DOMAIN_NAME is not provided, then v2ray would not be set to websocket."
+    exit -1
+elif [ ${#} -eq 1 ]; then
+    DOMAIN_NAME=${1}
+    V2RAY_CONFIG_FILE=v2ray_config_ws.json
+else
+    read -p "Enter nginx site name: " DOMAIN_NAME
+    V2RAY_CONFIG_FILE=v2ray_config.json
+fi
 
 ### Update home config
 cp ${SCRIPT_PATH}/../.gitconfig ${HOME_PATH}/
@@ -23,7 +38,7 @@ EOF
 
 ### Update and install software
 sudo apt-get update && sudo apt-get upgrade -y
-sudo apt-get install net-tools ntpdate shellinabox nginx -y
+sudo apt-get install net-tools ntpdate openssl python3-virtualenv shellinabox nginx -y
 
 ### Set timezone and synchronize with ntp server
 sudo ntpdate -u ntp.ubuntu.com
@@ -53,10 +68,44 @@ sudo sysctl -p
 #sysctl net.ipv4.tcp_congestion_control
 #lsmod | grep bbr
 
-### Install and config v2ray
-cd ${SCRIPT_PATH}/../v2ray
-sudo bash v2ray_auto.sh
-cd ${SCRIPT_PATH}
+### Config jupyter notebook
+NOTEBOOK_PATH=${HOME_PATH}/notebook
+NOTEBOOK_WORK_PATH=${HOME_PATH}/${DOMAIN_NAME}/notebook
+NOTEBOOK_CONFIG_FILE=${HOME_PATH}/.jupyter/jupyter_notebook_config.py
+
+mkdir -p ${NOTEBOOK_PATH}
+mkdir -p ${NOTEBOOK_WORK_PATH}
+sudo chmod 777 ${NOTEBOOK_WORK_PATH}
+virtualenv ${NOTEBOOK_PATH}
+source ${NOTEBOOK_PATH}/bin/activate
+pip3 install jupyter
+jupyter notebook --generate-config
+echo "[Set jupyter notebook password]"
+jupyter notebook password
+deactivate
+sudo sed -i "s|# c.NotebookApp.ip = 'localhost'|c.NotebookApp.ip = '0.0.0.0'|g" ${NOTEBOOK_CONFIG_FILE}
+sudo sed -i "s|# c.NotebookApp.port = 8888|c.NotebookApp.port = 4400|g" ${NOTEBOOK_CONFIG_FILE}
+sudo sed -i "s|# c.NotebookApp.base_url = '/'|c.NotebookApp.base_url = '/nb'|g" ${NOTEBOOK_CONFIG_FILE}
+sudo sed -i "s|# c.NotebookApp.allow_origin = ''|c.NotebookApp.allow_origin = '*'|g" ${NOTEBOOK_CONFIG_FILE}
+echo "[Unit]
+Description=Jupyter Notebook
+
+[Service]
+Type=simple
+PIDFile=/run/notebook.pid
+ExecStart=${NOTEBOOK_PATH}/bin/jupyter-notebook --config=${NOTEBOOK_CONFIG_FILE}
+User=ubuntu
+Group=ubuntu
+WorkingDirectory=${NOTEBOOK_WORK_PATH}
+Restart=always
+RestartSec=10
+
+[Install]
+WantedBy=multi-user.target" | sudo tee /etc/systemd/system/notebook.service > /dev/null
+
+sudo systemctl daemon-reload
+sudo systemctl enable notebook
+sudo systemctl start notebook
 
 ### Config shellinabox
 sudo cat > /etc/default/shellinabox <<EOF
@@ -72,13 +121,20 @@ SHELLINABOX_PORT=4200
 SHELLINABOX_ARGS="--no-beep --disable-ssl"
 EOF
 
+sudo systemctl daemon-reload
 sudo systemctl enable shellinabox
 sudo systemctl start shellinabox
 
+### Install and config v2ray
+bash ${SCRIPT_PATH}/../v2ray/v2ray_auto.sh ${SCRIPT_PATH}/../v2ray/${V2RAY_CONFIG_FILE}
+
 ### Config nginx
-echo "If you have freenom and cloudflare, you can use nginx to proxy shellinabox and v2ray by:"
-echo "sudo bash v2ray_auto.sh CONF_FILE"
-echo "sudo bash nginx_init.sh SITE_CERT_PATH SITE_CONF_FILE [SITE_NAME]"
+if [[ ! -d ${CERT_PATH} || ! -f ${CERT_PATH}/site.key || ! -f ${CERT_PATH}/site.cert ]]; then
+    echo "Generating self signed certificate ..."
+    mkdir -p ${CERT_PATH}
+    openssl req -x509 -newkey rsa:4096 -nodes -out ${CERT_PATH}/site.cert -keyout ${CERT_PATH}/site.key -days 9999 -subj "/C=US/ST=California/L=SanJose/O=Global Security/OU=IT Department/CN=test@gmail.com"
+fi
+bash ${SCRIPT_PATH}/nginx_init.sh ${CERT_PATH} ${SCRIPT_PATH}/nginx.conf ${DOMAIN_NAME}
 
 ### SET PASSWORD
 echo "***** CHANGE PASSWORD FOR root & ubuntu *****"
