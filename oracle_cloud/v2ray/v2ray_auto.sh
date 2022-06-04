d9e126cec30a521c35ebe167a675e7d343cc4096
nsosnsos <nsosnsos@gmail.com>
2022-06-04 11:55:24 +0800

    FIX: CHANGE V2RAY ID
    change v2ray id by default from input.

--- oracle_cloud/v2ray/v2ray_auto.sh
+++ oracle_cloud/v2ray/v2ray_auto.sh
@@ -21,5 +21,9 @@ sudo bash ${SCRIPT_PATH}/v2ray_install.sh
 sudo bash ${SCRIPT_PATH}/v2ray_dat_install.sh
 
 sudo cp ${1} /usr/local/etc/v2ray/config.json
+echo "Default v2ray id: 00000000-0000-0000-0000-000000000000"
+echo "Please modify to v2ray config id by [uuidgen]"
+read -p "Enter v2ray id: " V2RAY_ID
+sudo sed -i "s|00000000-0000-0000-0000-000000000000|${V2RAY_ID}|g" /usr/local/etc/v2ray/config.json
 sudo systemctl restart v2ray
 
